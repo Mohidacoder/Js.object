3ea6f2cc5969ccd1c53d26f630024b082a92e879
Elmo'minova Mohida <mashinada123@gmail.com>
2024-02-19 20:58:22 +0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,66 @@
+"use strict";
+
+//-------------------------------first task---------------------------
+function numbers (a , b){
+    return (a+b)<=100
+}
+const calc = numbers(22,15);
+console.log(calc);
+
+function rate (a , b){
+    return (a+b)<=100
+}
+const call = rate(83,34);
+console.log(call);
+
+//-------------------------------second task---------------------------
+
+function remain (a,b){
+    return a % b
+}
+const division = remain(1,3);
+console.log(division);
+
+/*************  2-type **************/
+
+function age (a,b){
+    return a % b
+}
+const plus = age(3 , 4);
+console.log(plus);
+
+/*************  3-type **************/
+
+function young (a,b){
+    return a % b
+}
+const equal = young(-9, 45);
+console.log(equal);
+
+
+//-------------------------------third task----------------------------------
+
+function room(a,b){
+    return a * b
+}
+const home = room(1,6);
+console.log(home);
+
+/*************  3-type **************/
+
+function rooms(a ,b){
+    return (a * b)-3
+}
+const homes = rooms(4, 6);
+console.log(homes)
+
+/*************  3-type **************/
+function cup (a,b){
+    return (a * b)-86
+}
+const door = cup(87, 6);
+console.log(door);
+
+
+
+
